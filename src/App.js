36a9b969dbed37691c21feff9c0fc30c67656c22
BryanHo10 <bryankho1010@gmail.com>
2020-11-29 19:26:12 -0800

    Add  page content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/404";
 
 function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route path="/">
+				<Route exact path="/">
 					<Login />
 				</Route>
-				{/* <Route path="/">
-						<Login />
-					</Route>
-          <Route path="/">
-						<Login />
-					</Route> */}
+				<Route exact path="/home">
+					<Dashboard />
+				</Route>
+				<Route>
+					<NotFound />
+				</Route>
 			</Switch>
 		</Router>
 	);
