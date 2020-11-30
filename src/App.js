6b46a4a7af61a9a95da0182c66b9dd90d4fcd851
BryanHo10 <bryankho1010@gmail.com>
2020-11-29 19:09:47 -0800

    Add simple login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route path="/">
+					<Login />
+				</Route>
+				{/* <Route path="/">
+						<Login />
+					</Route>
+          <Route path="/">
+						<Login />
+					</Route> */}
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
